8f7fc35a4aca3db15c3077a406f1571370d6d43f
henry <70426440+hyjoong@users.noreply.github.com>
2023-08-02 01:08:07 +0900

    refactor: button component 임시 삭제

--- components/ui/Button/type.ts
+++
@@ -1,8 +0,0 @@
-import { VariantProps } from "class-variance-authority";
-import { buttonVariants } from "./style";
-
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
-  asChild?: boolean;
-}
