54ab3ffbcf53a3403c227b1b28174d5cc4677513
henry <70426440+hyjoong@users.noreply.github.com>
2023-08-02 21:32:31 +0900

    refactor: navbar component 폴더 위치 변경 - common 폴더 하위구조로 변경

--- components/common/navbar/index.tsx
+++ components/common/navbar/index.tsx
@@ -5,11 +5,11 @@ import { UserButton } from "@clerk/nextjs";
 import { Menu, Sparkles } from "lucide-react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { ModeToggle } from "./common/modeToggle";
+import { ModeToggle } from "../modeToggle";
 
 const Navbar = () => {
   return (
-    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
       <div className="flex items-center">
         <Menu className="block md:hidden" />
         <Link href="/">
